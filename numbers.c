ee8f0faead29d7e73c181e3f87b0602dc8de3f2b
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-04-30 18:44:34 +0530

    created list.c and made some initial changes to existing files
    * added main to gitignore
    * defined MENU macro in numbers.c
    * modified type of count from int to size_t(unsignd long) in List struct

--- numbers.c
+++ numbers.c
@@ -1,4 +1,23 @@
+#include <stdio.h>
+#include "list.h"
+
+#define MENU "Main Menu\n---------\n\
+(a) add a number to the end of the list\n\
+(b) add a number to the start of the list\n\
+(c) insert a number at a given position in the list\n\
+(d) add a unique item on the list at the end\n\
+(e) remove a number from the beginning of the list\n\
+(f) remove a number from the end of the list\n\
+(g) remove a number from a given position in the list\n\
+(h) remove first occurrence of a number\n\
+(i) remove all occurrences of a number\n\
+(j) clear the whole list\n\
+(k) check if a number exists in the list\n\
+(l) display the list of numbers\n\
+(m) exit\n\nPlease enter the alphabet of the operation you would like to perform\n"
+
 int main(void)
 {
+  printf("%s", MENU);
   return 0;
 }
