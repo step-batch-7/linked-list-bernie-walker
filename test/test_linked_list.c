0bd7ae4560b155be68e6e1313cea207b5e51f735
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-05-06 21:04:59 +0530

    added tests for add_to_end

--- test/test_linked_list.c
+++ test/test_linked_list.c
@@ -1,6 +1,16 @@
 #include "test.h"
 #include "../list.h"
 
+void test_add_to_end(void)
+{
+  List_ptr list = create_list();
+  assert_strict_equal("Element should be successfully added when memory is available", add_to_end(list, 8), Success);
+  assert_strict_equal("Added element should be the value of the last", list->last->value, 8);
+  assert_strict_equal("When the list is empty the added element should be the first element as well", list->last->value, 8);
+  add_to_end(list, 6);
+  assert_strict_equal("The element should be added to the ending of the list", list->last->value, 6);
+}
+
 void test_add_to_start(void)
 {
   List_ptr list = create_list();
@@ -14,5 +24,6 @@ void test_add_to_start(void)
 int main(void)
 {
   exec_test_suite("add_to_start", test_add_to_start);
+  exec_test_suite("add_to_end", test_add_to_end);
   return 0;
 }
