a6ea782a9a80b0ccaf852bdf22078a99af6af062
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-05-07 11:43:26 +0530

    updated previously written tests

--- test/test_linked_list.c
+++ test/test_linked_list.c
@@ -24,6 +24,7 @@ void test_add_unique(void)
   List_ptr list = create_list();
   assert_strict_equal("Should add any element to an empty list", add_unique(list, 3), Success);
   assert_strict_equal("Should add a unique item to the list", add_unique(list, 2), Success);
+  assert_strict_equal("The count of the list should be updated", list->count, 2);
   assert_strict_equal("Should not add a non unique item to the list", add_unique(list, 2), Failure);
   assert_strict_equal("Should add item at the end of the list", list->last->value, 2);
 }
@@ -35,6 +36,7 @@ void test_insert_at(void)
   assert_strict_equal("Should add at at the starting of the list", list->head->value, 1);
   insert_at(list, 2, 1);
   assert_strict_equal("Should add at at the end of the list", list->last->value, 2);
+  assert_strict_equal("The count of the list should be updated", list->count, 2);
   insert_at(list, 3, 1);
   assert_strict_equal("Should add in the middle of the list", list->head->next->value, 3);
   assert_strict_equal("Should not add at an invalid postion", insert_at(list, 6, 9), Failure);
@@ -45,6 +47,7 @@ void test_add_to_end(void)
   List_ptr list = create_list();
   assert_strict_equal("Element should be successfully added when memory is available", add_to_end(list, 8), Success);
   assert_strict_equal("Added element should be the value of the last", list->last->value, 8);
+  assert_strict_equal("The count of the list should be updated", list->count, 1);
   assert_strict_equal("When the list is empty the added element should be the first element as well", list->last->value, 8);
   add_to_end(list, 6);
   assert_strict_equal("The element should be added to the ending of the list", list->last->value, 6);
@@ -55,6 +58,7 @@ void test_add_to_start(void)
   List_ptr list = create_list();
   assert_strict_equal("Element should be successfully added when memory is available", add_to_start(list, 9), Success);
   assert_strict_equal("Added element should be the value of the head", list->head->value, 9);
+  assert_strict_equal("The count of the list should be updated", list->count, 1);
   assert_strict_equal("When the list is empty the added element should also be the last element", list->last->value, 9);
   add_to_start(list, 7);
   assert_strict_equal("The element should be added to the starting of the list", list->head->value, 7);
@@ -65,7 +69,7 @@ void test_has_element(void)
   List_ptr list = create_list();
   assert_strict_equal("Should not find the element which does not exist", has_element(list, 1), Failure);
   add_unique(list, 2);
-  assert_strict_equal("Should not find the element which does not exist", has_element(list, 2), Success);
+  assert_strict_equal("Should find the element which exists", has_element(list, 2), Success);
 }
 
 int main(void)
